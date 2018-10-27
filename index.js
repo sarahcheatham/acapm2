a6a73f9f84a95b9ac85c8797509c0b953b964717
sarahcheatham <sarah.cheatham14@gmail.com>
2018-10-27 12:50:01 -0500

    new acapm repo

---
+++ index.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+//checks if 2nd argument is 'init'
+process.argv[2] === 'init'
+
+  rl.question('App name?', (name)=>{
+    rl.question('Description?', (desc)=>{
+      rl.question('Version?', (version)=>{
+        rl.question('Author?', (author)=>{
+          rl.question('Main file path?', (main)=>{
+            const response = {
+              'name': name,
+              'description': desc,
+              'version': version,
+              'author': author,
+              'main': main
+            }
+
+            const obj = JSON.stringify(response, '', '\t')
+            fs.writeFile('package.json', obj, (err)=>{
+              if(err) throw err;
+              console.log('A new package.json file has been saved');
+            });
+            rl.close()
+          })
+        })
+      })
+    })
+  })
+
+
+  
